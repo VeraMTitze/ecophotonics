65d16902baf1d1a24841778800164f636c9d7270
VeraMTitze <66787179+VeraMTitze@users.noreply.github.com>
2026-02-23 22:47:21 +0100

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'outreach']
+const section_names = ['home', 'people','publications', 'outreach']
 
 
 window.addEventListener('DOMContentLoaded', event => {
